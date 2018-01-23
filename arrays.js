f7c8165fad60175da8b90185457388694b66611a
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-01-23 00:59:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,5 +32,5 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(array.length - 1);
+  return array.slice(0, array.length - 1);
 }
